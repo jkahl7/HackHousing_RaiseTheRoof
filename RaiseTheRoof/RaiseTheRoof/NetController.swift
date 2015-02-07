211edc6103df7065bd66132b97e0b8c35230f121
Josh Kahl <j7kahl@gmail.com>
2015-02-07 11:31:41 -0800

    initial commit

---
+++ RaiseTheRoof/RaiseTheRoof/NetController.swift
@@ -0,0 +1,42 @@
+//
+//  NetController.swift
+//  RaiseTheRoof
+//
+//  Created by Josh Kahl on 2/7/15.
+//  Copyright (c) 2015 Josh Kahl. All rights reserved.
+//
+
+import Foundation
+
+
+class NetController
+{
+  
+  class var sharedNetController : NetController
+  {
+  
+    struct Static
+    {
+      static let instance : NetController = NetController()
+    }
+    return Static.instance
+  }
+  
+  var urlSession = NSURLSession()
+  var accessToken : String?
+  
+  init()
+  {
+    let ephermeralConfigeration = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
